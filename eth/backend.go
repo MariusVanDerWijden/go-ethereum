331ddfbd0442178bbfa57d95bb120d97d324c2aa
MariusVanDerWijden <m.vanderwijden@live.de>
2026-02-09 14:46:55 +0100

    eth: hotfix debug_setHead, does not address the underlying issue

--- eth/backend.go
+++ eth/backend.go
@@ -495,6 +495,9 @@ func (s *Ethereum) updateFilterMapsHeads() {
 		if head == nil || newHead.Hash() != head.Hash() {
 			head = newHead
 			chainView := s.newChainView(head)
+			if chainView == nil {
+				return // This can happen during debug_setHead
+			}
 			historyCutoff, _ := s.blockchain.HistoryPruningCutoff()
 			var finalBlock uint64
 			if fb := s.blockchain.CurrentFinalBlock(); fb != nil {
